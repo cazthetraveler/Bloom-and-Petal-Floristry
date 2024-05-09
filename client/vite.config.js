51a21fc678b812cf3798ae964efd899966012be0
Luis Olmos <137588686+cazthetraveler@users.noreply.github.com>
2024-05-09 09:40:57 -0500

    changed it back

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
@@ -7,11 +7,11 @@ export default defineConfig({
     port: 3000,
     open: true,
     proxy: {
-      '/graphql': {
-        target: process.env.API_URL || 'http://localhost:3001',
-        secure: true,
-        changeOrigin: true
-      }
-    }
-  }
+      "/graphql": {
+        target: "http://localhost:3001",
+        secure: false,
+        changeOrigin: true,
+      },
+    },
+  },
 });
